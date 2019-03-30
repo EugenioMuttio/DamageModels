c8275f78f0f5fcb81156686c0bd578c5dffffb9d
Eugenio <ej.muttiozavala@ugto.mx>
2019-03-30 12:32:27 +0100

    Tests

--- main_nointeractive.m
+++ main_nointeractive.m
@@ -16,16 +16,16 @@ clear all
 
 % YOUNG's MODULUS
 % ---------------
-YOUNG_M = 20000 ;
+YOUNG_M = 2.00E+11 ;
 % Poisson's coefficient
 % -----------------------
-POISSON = 0.3 ;
+POISSON = 0.26 ;
 % Hardening/softening modulus
 % ---------------------------
-HARDSOFT_MOD = -0.1 ;
+HARDSOFT_MOD = -0.2 ;
 % Yield stress
 % ------------
-YIELD_STRESS = 200 ;
+YIELD_STRESS = 2.50E+08 ;
 % Problem type  TP = {'PLANE STRESS','PLANE STRAIN','3D'}
 % ------------------------ = 1            =2         =3
 % ------------
@@ -33,16 +33,16 @@ ntype= 2 ;
 % Model    PTC = {'SYMMETRIC','TENSION','NON-SYMMETRIC'} ;
 %                     = 1         = 2         = 3
 % ---------------------------------------------------
-MDtype =1;
+MDtype =3;
 % Ratio compression strength / tension strength
 % ---------------------------------------------
-n = 2 ;
+n = 5 ;
 % SOFTENING/HARDENING TYPE
 % ------------------------
 HARDTYPE = 'EXPONENTIAL' ; %{LINEAR,EXPONENTIAL}
 % VISCOUS/INVISCID
 % ------------------------
-VISCOUS = 'YES' ;
+VISCOUS = 'NO' ;
 % Viscous coefficient ----
 % ------------------------
 eta = 0.1 ;
@@ -56,9 +56,9 @@ ALPHA_COEFF = 0.5 ;
 % ----------------------------------
 nloadstates = 3 ;
 SIGMAP = zeros(nloadstates,2) ;
-SIGMAP(1,:) =[150 0];
-SIGMAP(2,:) =[300 0];
-SIGMAP(3,:) =[450 0];
+SIGMAP(1,:) =[3.75E+08 0];
+SIGMAP(2,:) =[1.88E+08 0];
+SIGMAP(3,:) =[6.88E+08 0];
 % Number of time increments for each load state
 % --------------------------------------- 
 istep = 10*ones(1,nloadstates) ;
